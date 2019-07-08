430406b7cb14ebde9ce631ba0f2ab1b0225ccdb4
caldito <100383255@alumnos.uc3m.es>
2019-07-08 15:39:10 +0200

    arreglo docker push

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,12 @@ docker build -t caldito99/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t caldito99/multi-worker:latest -f ./worker/Dockerfile ./worker
 docker build -t caldito99/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push caldito99 multi-client:latest
-docker push caldito99 multi-client:$SHA
-docker push caldito99 multi-server:latest
-docker push caldito99 multi-server:$SHA
-docker push caldito99 multi-worker:latest
-docker push caldito99 multi-worker:$SHA
+docker push caldito99/multi-client:latest
+docker push caldito99/multi-client:$SHA
+docker push caldito99/multi-server:latest
+docker push caldito99/multi-server:$SHA
+docker push caldito99/multi-worker:latest
+docker push caldito99/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=caldito99/multi-server:$SHA
